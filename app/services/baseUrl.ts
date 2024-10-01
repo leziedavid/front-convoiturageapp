859b6bb5acfa82c4e7061a594b1aab37d6bbadd8
quentin103 <koffi.kouamelan.yq@gmail.com>
2024-10-01 15:06:55 +0000

    ​add docker-compose

--- app/services/baseUrl.ts
+++ app/services/baseUrl.ts
@@ -1,6 +1,6 @@
 // baseUrl.ts
 
-const BASE_URL_DEV = 'http://localhost:4000/api';
+const BASE_URL_DEV = 'http://195.110.35.237:4000/api';
 const BASE_URL_PROD = 'https://backend.covoitivoire.com/api';
 
 export const getBaseUrl = (): string => {
